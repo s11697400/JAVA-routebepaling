d73dc5dde25d9c775ef3b25ddcb77da23a2f7135
Thijs Soepenberg <tsoepenberg@gmail.com>
2022-05-11 16:20:42 +0200

    Berekening van routes uitgevoerd

--- src/Route.java
+++ src/Route.java
@@ -1,3 +1,8 @@
-public class Route {
+import java.util.ArrayList;
 
+public class Route {
+    public ArrayList<Tussenstop> Tussenstops = new ArrayList<Tussenstop>();
+    private int RouteID;
+    private float afstand;
+    private float berekentijd;
 }
--- src/Tussenstop.java
+++ src/Tussenstop.java
@@ -1,2 +1,7 @@
 public class Tussenstop {
+    private String Adress;
+    private float lat;
+    private float lon;
+    private int TussenStopID;
+
 }
